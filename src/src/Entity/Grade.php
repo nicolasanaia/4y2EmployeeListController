1f3200a7cc9617d746f431dc7790c8ea657e005b
nicolasanaia <nicolasanaia@gmail.com>
2021-04-26 12:59:03 -0300

    version 9.1

--- src/src/Entity/Grade.php
+++ src/src/Entity/Grade.php
@@ -4,6 +4,7 @@ namespace App\Entity;
 
 use App\Repository\GradeRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass=GradeRepository::class)
